874b6a888e82afc5dcff2875533a10fb90df50d6
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-23 03:26:44 +0100

    Fix small header issues

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { DarkModeToggle } from "./DarkModeToggle";
 import { SearchBar } from "./SearchBar";
 import Link from "next/link";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const MenuItems = ({ children, href }) => (
   <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
@@ -29,27 +30,19 @@ export const Header = () => {
       </Flex>
 
       <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
-        <svg
-          fill="white"
-          width="12px"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
+        <HamburgerIcon />
       </Box>
 
       <Box
-        display={{ sm: show ? "block" : "none", md: "flex" }}
-        width={{ sm: "full", md: "auto" }}
+        display={{ base: show ? "block" : "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
         alignItems="center"
         flexGrow={1}
       >
         <MenuItems href="/dashboard">Dashboard</MenuItems>
         <MenuItems href="/stats">Stats</MenuItems>
         <Box
-          display={{ md: "flex" }}
+          display={{ base: "block", md: "flex" }}
           position={{ base: "relative", md: "absolute" }}
           right={{ md: "1rem" }}
         >
