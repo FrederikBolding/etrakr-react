c66a8c2fe154936bc20d1523d1e6d7841282097b
Frederik Bolding <frederik.bolding@gmail.com>
2023-03-01 15:04:34 +0100

    Swap out API URL

--- api/tmdb.ts
+++ api/tmdb.ts
@@ -1,3 +1,3 @@
 import { MovieDb } from 'moviedb-promise'
 
-export const tmdb = new MovieDb('API_KEY', 'https://etrakr-worker.frederik.workers.dev/tmdb/')
+export const tmdb = new MovieDb('API_KEY', 'https://api.etrakr.net/tmdb/')
